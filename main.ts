0b9108145f76389cd31a3ad53229b512cdd8ecc6
Araden <2713886816@qq.com>
2023-08-26 13:36:46 +0800

    feat: 自动删除不需要的图片资源

--- main.ts
+++ main.ts
@@ -1,5 +1,4 @@
-import { access, mkdir, writeFile } from 'node:fs/promises'
-import { Buffer } from 'node:buffer'
+import { access, mkdir, readdir, rm, writeFile } from 'node:fs/promises'
 import puppeteer, { Page } from 'puppeteer'
 import { children } from './bookmarks.json'
 
@@ -9,10 +8,11 @@ interface BookmarkItem {
   children?: BookmarkItem[]
 }
 
-const IGNORE = new Set(['', 'lab', '个人网站', '杂项', '学校网站', '博文收藏', '简历投递链接'])
+const IGNORE = new Set(['', 'lab', '个人网站', '杂项', '学校网站', '博文收藏', '简历投递链接', '流程中'])
 const VIEWPORT = { width: 1280, height: 720 }
-const WAITTIME = 500
+const WAITTIME = 5000
 
+// 从浏览器导出书签中提取要截图的那部分
 function purifying(origin: BookmarkItem) {
   if (IGNORE.has(origin.title)) return
 
@@ -34,6 +34,7 @@ function purifying(origin: BookmarkItem) {
   return copied
 }
 
+// 根据给定的 uri 截图，放入 path 下
 async function snap(page: Page, uri: string, path: string) {
   const startTime = +new Date()
 
@@ -49,45 +50,91 @@ async function snap(page: Page, uri: string, path: string) {
 
     console.log(`${path} 截图成功, 耗时 ${finishTime - startTime}ms`)
   } catch (e) {
-    console.log(`${uri} 截图失败, 捕获到${e}`)
+    console.log(`${path} ${uri} 截图失败, 捕获到${e}`)
   }
 }
 
-async function traverse(page: Page, navigations: BookmarkItem[], path: string[]) {
-  for (const navi of navigations) {
-    path.push(navi.title)
-    const dirPath = path.join('/')
+// 递归遍历导航对象，根据对象类型选择截图或者递归遍历子数组
+async function traverse(page: Page, navi: BookmarkItem, path: string[]) {
+  path.push(navi.title)
+  const dirPath = path.join('/')
 
-    if (navi.uri) {
-      // 有 URL 则截图
-      try {
-        await access(`${dirPath}.png`)
-      } catch (e) {
-        await snap(page, navi.uri, `${dirPath}.png`)
-      }
-    } else {
-      // 无 URL 创建文件夹，递归
-      try {
-        await access(dirPath)
-      } catch (e) {
-        await mkdir(dirPath, { recursive: true })
-        console.log(`创建文件夹：${dirPath}\n`)
-      } finally {
-        await traverse(page, navi.children, path)
+  if (navi.uri) {
+    // 有 URL 则截图
+    try {
+      await access(`${dirPath}.png`)
+    } catch (e) {
+      await snap(page, navi.uri, `${dirPath}.png`)
+    }
+  } else if (navi.children) {
+    // 无 URL 创建文件夹，递归
+    try {
+      await access(dirPath)
+    } catch (e) {
+      await mkdir(dirPath, { recursive: true })
+      console.log(`创建文件夹：${dirPath}\n`)
+    } finally {
+      for (const child of navi.children) {
+        await traverse(page, child, path)
       }
     }
-
-    path.pop()
   }
+
+  path.pop()
 }
 
-const main = async (navigations: BookmarkItem[]) => {
+// 得到当前导航对象下所有截图的路径集合，用于删除不需要的截图
+function fileSet(navi: BookmarkItem, path: string[] = [], res = new Set<string>()) {
+  path.push(navi.title)
+
+  if (navi.children) {
+    for (const child of navi.children) {
+      fileSet(child, path, res)
+    }
+  } else if (navi.uri) {
+    res.add(`${path.join('/')}.png`)
+  }
+
+  path.pop()
+  return res
+}
+
+// 递归遍历生成的截图文件夹，删除导航对象中没有的截图文件
+async function remove(navi: BookmarkItem, path: string[], existed: Set<string>) {
+  path.push(navi.title)
+
+  const files = await readdir(path.join('/'))
+
+  for (const file of files) {
+    if (file.endsWith('.png')) {
+      const fullPath = `${path.join('/')}/${file}`
+      if (!existed.has(fullPath)) {
+        try {
+          await rm(fullPath)
+          console.log(`${fullPath} 删除成功`)
+        } catch (err) {
+          console.log(`${fullPath} 删除失败，请手动删除`)
+        }
+      }
+    } else {
+      await remove(
+        navi.children.find(child => child.title === file),
+        path,
+        existed
+      )
+    }
+  }
+
+  path.pop()
+}
+
+// 程序本体
+const main = async (navigations: BookmarkItem) => {
   try {
     // 写入文件
     const { signal } = new AbortController()
-    const data = new Uint8Array(Buffer.from(JSON.stringify(navigations)))
-    await writeFile('navigator.json', data, { signal })
-    console.log('提取 navigator.json 成功！')
+    await writeFile('navigator.ts', `export const navigator = ${JSON.stringify(navigations)}`, { signal })
+    console.log('提取 navigator.ts 成功！')
 
     // 创建浏览器，打开窗口
     const browser = await puppeteer.launch({ headless: 'new' })
@@ -97,7 +144,10 @@ const main = async (navigations: BookmarkItem[]) => {
     await page.setViewport(VIEWPORT)
 
     // 开始截图
-    await traverse(page, navigations, ['navigator'])
+    await traverse(page, navigations, [])
+
+    // 移除导航对象中不包含的截图
+    await remove(navigations, [], fileSet(navigations))
 
     await page.close()
     await browser.close()
@@ -106,7 +156,7 @@ const main = async (navigations: BookmarkItem[]) => {
   }
 }
 
-const toolbar = children.find(child => child.title === 'toolbar')
-const navigations = purifying(toolbar).children
+const navigations = purifying(children.find(child => child.title === 'toolbar'))
+navigations.title = 'navigator'
 
 main(navigations)
